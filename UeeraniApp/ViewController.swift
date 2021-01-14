4de0bb88287130d61cfa5c792dc7fbf5716d451c
Rosario Munoz <rossmg1996@gmail.com>
2021-01-13 23:25:40 -0600

    View login,registro,bienvenida

--- UeeraniApp/ViewController.swift
+++ UeeraniApp/ViewController.swift
@@ -9,17 +9,16 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    @IBOutlet weak var speechText: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let utterance = AVSpeechUtterance(string: speechText.text!)
+        /*let utterance = AVSpeechUtterance(string: speechText.text!)
         utterance.voice = AVSpeechSynthesisVoice(language: "es-mx")
         utterance.rate = 0.5
 
         let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
+        synthesizer.speak(utterance)*/
     }
 
 
