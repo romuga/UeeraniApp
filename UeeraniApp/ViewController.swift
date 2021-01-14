dc442212cf8ce34c6ac9fbbfb80dabffc4d2f939
Rosario Munoz <rossmg1996@gmail.com>
2021-01-13 20:51:44 -0600

    Pruebas text-to-speech AVFoundation

--- UeeraniApp/ViewController.swift
+++ UeeraniApp/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var speechText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let utterance = AVSpeechUtterance(string: speechText.text!)
+        utterance.voice = AVSpeechSynthesisVoice(language: "es-mx")
+        utterance.rate = 0.5
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
     }
 
 
