b2f7f1299f664349a951dfc7b97c09e2274db0b1
Rosario Munoz <rossmg1996@gmail.com>
2021-01-14 23:02:07 -0600

    add view perfil e images

---
+++ UeeraniApp/RegistroViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegistroViewController.swift
+//  UeeraniApp
+//
+//  Created by Ross on 14/01/21.
+//
+
+import UIKit
+
+class RegistroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
